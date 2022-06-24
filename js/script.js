a1c741e81708b38c87e3bf3c837887f2ffce8071
Debra Hill <dhilldesign@suddenlink.net>
2022-06-23 22:54:31 -0500

    DHill GitHub Repo Gallery

---
+++ js/script.js
@@ -0,0 +1,38 @@
+//selecting profile informatin div class
+const overview = document.querySelector (".overview");
+const username = "debbieh-20";
+
+//fetch api json data
+const gitUserInfo = async function () {
+  //fetch api and target the users endpoint; use a template literal to add the global username variable to the endpoint
+  const userInfo = await fetch(`https://api.github.com/users/${username}`); //added a $ character in front of the variable name to create a placeholder
+
+  //resolve the json response
+  const data = await userInfo.json();
+  // call the function displaying the user information
+  displayUserInfo(data);
+};
+
+gitUserInfo();
+  
+  // a new function to display the fetched user information on the page. Accepts the JSON data as a parameter.
+
+const displayUserInfo = function(data) {
+  //create a new div and give it a class of 'user-info'
+  const div = document.createElement("div");
+  div.classList.add("user-info");
+  //populate the div with the elements for figure, image and paragraphs
+  div.innerHTML = 
+  `<figure>
+  <img alt="user avatar" src=${data.avatar_url} />
+  </figure>
+  <div>
+    <p><strong>Name:</strong> ${data.name}</p>
+    <p><strong>Bio:</strong> ${data.bio}</p>
+    <p><strong>Location:</strong> ${data.location}</p>
+    <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+  </div>`;
+  overview.append(div);
+};
+
+
