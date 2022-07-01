7c743d7d8841e127dca3ab76a81b1f8a4eff726c
Debra Hill <dhilldesign@suddenlink.net>
2022-07-01 18:25:02 -0500

    Repo Gallery Step 3

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
-//selecting profile informatin div class
+//selecting profile information div class
 const overview = document.querySelector (".overview");
 const username = "debbieh-20";
+//to select the unordered lit to display the repos list
+const repoList = document.querySelector (".repo-list");
 
 //fetch api json data
 const gitUserInfo = async function () {
@@ -24,7 +26,7 @@ const displayUserInfo = function(data) {
   //populate the div with the elements for figure, image and paragraphs
   div.innerHTML = 
   `<figure>
-  <img alt="user avatar" src=${data.avatar_url} />
+    <img alt="user avatar" src=${data.avatar_url} />
   </figure>
   <div>
     <p><strong>Name:</strong> ${data.name}</p>
@@ -33,6 +35,22 @@ const displayUserInfo = function(data) {
     <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
   </div>`;
   overview.append(div);
+  gitRepos();
 };
 
-
+const gitRepos = async function () {
+  //sort repos by the most recenly used to last updated; show up to 100 repos per page at a time
+  const fetchRepos = await 
+    fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+      const repoData = await fetchRepos.json();
+      displayRepos(repoData);
+};
+//display info about your repos
+const displayRepos = function (repos) {
+  for (const repo of repos) {
+    const repoItem = document.createElement("li");
+    repoItem.classList.add("repo");
+    repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+    repoList.append(repoItem);
+  }
+};
